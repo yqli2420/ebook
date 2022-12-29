cf12ef9a02dff124925fc042efab9f028c931e61
yongqiang li <yongqiangli@alumni.hust.edu.cn>
2022-12-29 20:30:35 +0800

    c

---
+++ source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'yqlibook'
+copyright = '2022, liyongqiang'
+author = 'liyongqiang'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser"
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+import myst_parser
+
+source_parsers = {
+    '.md': myst_parser
+}
+source_suffix = ['.rst', '.md']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+#html_static_path = ['_static']
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+#html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': False,
+    'navigation_depth': 3,
+}
+html_logo = 'images/logo.png'
---
+++ source/index.rst
@@ -0,0 +1,14 @@
+.. yqlibook documentation master file, created by
+   sphinx-quickstart on Thu Dec 29 15:16:23 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+机器学习与语音书籍!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: 目录
+
+   ml
+   speech
